c884aec662506fbe4a48369744883addaeab7b99
Jason Lengstorf <jason@lengstorf.com>
2019-09-03 14:47:42 -0700

    fix: use @SaraVieira’s meme API instead

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,6 +2,6 @@ import ApolloClient from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
 
 export const client = new ApolloClient({
-  uri: 'https://nx9zvp49q7.lp.gql.zone/graphql',
+  uri: 'https://api-euwest.graphcms.com/v1/cjke2kn7p00ol01d2pinkptdj/master',
   fetch,
 });
