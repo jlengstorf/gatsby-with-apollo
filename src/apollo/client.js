c20c7daec39788f60f5df2967d4049ce170b7bf7
Jimmy Delas <jimmy.delas@gmail.com>
2020-07-25 01:08:02 +0200

    feat: upgrade packages and migrate to apollo 3 (#13)

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,10 @@
-import ApolloClient from 'apollo-boost';
-import fetch from 'isomorphic-fetch';
+import fetch from 'cross-fetch';
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-euwest.graphcms.com/v1/cjke2kn7p00ol01d2pinkptdj/master',
-  fetch,
+  link: new HttpLink({
+    uri: 'https://api-euwest.graphcms.com/v1/cjke2kn7p00ol01d2pinkptdj/master',
+    fetch,
+  }),
+  cache: new InMemoryCache()
 });
--- src/apollo/wrap-root-element.js
+++ src/apollo/wrap-root-element.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider } from '@apollo/client';
 import { client } from './client';
 
 export const wrapRootElement = ({ element }) => (
